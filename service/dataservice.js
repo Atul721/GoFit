28cf0ca6626103c972ce46aec3fc79120423e51f
Atul721 <atul7721@gmail.com>
2022-11-16 20:57:37 +0000

    Committing the Json file

---
+++ service/dataservice.js
@@ -0,0 +1,74 @@
+var data= [
+        {
+        "id": 1,
+        "img": ["../images/Dumbell.jpg","../images/Dumbell.jpg","../images/Dumbell.jpg"],
+        "title": "Dumbell",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        }, 
+        {
+        "id": 2,
+        "img": ["../images/Gym-ball.jpg"],
+        "title": "Gym Ball",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        },
+        { 
+        "id": 3,
+        "img": ["../images/Weight-Bar1.jpg"],
+        "title": "Weight Bar",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        },
+        {
+        "id": 4,
+        "img": ["../images/Threadmill.jpg"],
+        "title": "Treadmill",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        },
+        {
+        "id": 5,
+        "img": ["../images/Battle-rope.jpg"],
+        "title": "Battle Rope ",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        },
+        {
+        "id": 6,
+        "img": ["../images/Cable.jpg"],
+        "title": "Cable",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        },
+        {
+        "id": 7,
+        "img": ["../images/Boxing-bag.jpg"],
+        "title": "Boxing Bag",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        },
+        {
+        "id": 8,
+        "img": ["../images/kettle-bell.jpg"],
+        "title": "kettlebell",
+        "price": "$20",
+        "rating" : "4.5 Rating",
+        "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization. Dumbbell sets and racks are also ",
+        "video" : "../videos/dumbell.mp4"
+        }
+    ]
