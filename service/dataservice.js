d16d002fc7583bcc930c1b8252216d6e24810a80
Atul721 <atul7721@gmail.com>
2022-11-19 19:07:48 +0000

    Committing the changes and making the footer consistent

--- service/dataservice.js
+++ service/dataservice.js
@@ -3,7 +3,7 @@ var data= [
         "id": 1,
         "img": ["../images/Dumbell.jpg","../images/Dumbell1.jpg","../images/Dumbell2.jpg","../images/Dumbell3.jpg"],
         "title": "Dumbell",
-        "price": "$20",
+        "price": "€20",
         "rating" : "4.5 Rating",
         "description" : "We have the largest selection of dumbbells available anywhere! We have the dumbbells you require in round, hex, rubber, neoprene, vinyl, and urethane. A space- and money-efficient way to perform resistance training is using dumbbells. They are a superb practical instrument for loading virtually any action because they support 360 degrees of motion and demand a lot of stabilization.",
         "video" : "../videos/dumbell.mp4"
@@ -12,7 +12,7 @@ var data= [
         "id": 2,
         "img": ["../images/Gym-ball.jpg","../images/Gym-ball1.jpg","../images/Gym-ball2.jpg","../images/Gym-ball3.jpg"],
         "title": "Gym Ball",
-        "price": "$20",
+        "price": "€15",
         "rating" : "4.5 Rating",
         "description" : "With the aid of one of our gym balls or yoga balls, you may improve your at-home workouts and strengthen your core. Gym balls may be used for a wide variety of workouts, delivering extra pressure to the proper parts of your core to help you get more out of each rep. They are excellent for toning your muscles and improving physical endurance. ",
         "video" : "../videos/Gym-Ball.mp4"
@@ -21,7 +21,7 @@ var data= [
         "id": 3,
         "img": ["../images/Weight-Bar.jpg","../images/Weight-Bar1.jpg","../images/Weight-Bar2.jpg","../images/Weight-Bar3.jpg"],
         "title": "Weight Bar",
-        "price": "$20",
+        "price": "€50",
         "rating" : "4.5 Rating",
         "description" : "The GoFit Barbell is an elegant chrome knurled barbell that can be used at home. The bar weighs 20 kg and is 7 feet long. This barbell's diameter and length make it ideal for power racks, which cannot accommodate a 6' bar. The majority of weightlifters should be able to use this barbell because it can support loads of up to 700 lbs.",
         "video" : "../videos/Weight-Bar.mp4"
@@ -30,7 +30,7 @@ var data= [
         "id": 4,
         "img": ["../images/Treadmill.jpg","../images/Treadmill1.jpg","../images/Treadmill2.jpg","../images/Treadmill3.jpg"],
         "title": "Treadmill",
-        "price": "$20",
+        "price": "€300",
         "rating" : "4.5 Rating",
         "description" : "Smallest cardio equipment for home use, the GoFit Treadmill. The guilt of lounging around at home can be avoided by investing in a small treadmill when the weather, the air quality, or your mood don't let you to go to the gym.",
         "video" : "../videos/Treadmill.mp4"
@@ -39,7 +39,7 @@ var data= [
         "id": 5,
         "img": ["../images/Battle-rope.jpg","../images/Battle-rope1.jpg","../images/Battle-rope2.jpg","../images/Battle-rope3.jpg"],
         "title": "Battle Rope ",
-        "price": "$20",
+        "price": "€100",
         "rating" : "4.5 Rating",
         "description" : "Training battle ropes of the highest quality for intense workouts. These gym ropes may be used both indoors and outdoors because they are made of incredibly durable Poly Dacron. Heat-shrunk plastic handles offer a good grip (even when wet). Available lengths/thicknesses are 3x.",
         "video" : "../videos/Battle-rope.mp4"
@@ -48,7 +48,7 @@ var data= [
         "id": 6,
         "img": ["../images/Cable.jpg","../images/Cable1.jpg","../images/Cable2.jpg"],
         "title": "Cable",
-        "price": "$20",
+        "price": "€150",
         "rating" : "4.5 Rating",
         "description" : "This heavy duty Lat Pulldown Low Row, which is only available in GoFit, is manufactured from steel profiles measuring 80x60x2 mm and 50x50x2 mm. It has been tested for 200KG lifts. makes sure that training is safe. utilizing the Low pulley You can workout your back just like you would on specialized machinery.",
         "video" : "../videos/cable.mp4"
@@ -57,7 +57,7 @@ var data= [
         "id": 7,
         "img": ["../images/Boxing-bag.jpg","../images/Boxing-bag1.jpg","../images/Boxing-bag2.jpg"],
         "title": "Boxing Bag",
-        "price": "$20",
+        "price": "€220",
         "rating" : "4.5 Rating",
         "description" : "This 4ft punch bag is composed of sturdy fabric and comes with PU/PVC Gloves, making it great for perfecting your boxing techniques or just for overall fitness. It comes in a stunning red and black color scheme and is filled with 100% cotton fabric. Punch bags are perfect for boxing practice, helping to enhance speed, footwork, and agility, so get ready for a terrific aerobic workout.",
         "video" : "../videos/Boxing.mp4"
@@ -66,7 +66,7 @@ var data= [
         "id": 8,
         "img": ["../images/kettle-bell.jpg","../images/kettle-bell1.jpg","../images/kettle-bell2.jpg","../images/kettle-bell3.jpg"],
         "title": "kettlebell",
-        "price": "$20",
+        "price": "€25",
         "rating" : "4.5 Rating",
         "description" : "The Cast Iron Kettlebells from GoFit are exquisite works of cast iron. They are made to last a lifetime and are offered individually. Higher weights will soon be available. Don't depend on the gym being open. Purchase a set of the cast iron kettlebells from GoFit instead. They will undoubtedly satisfy the toughest requirements of your exercise regimens.",
         "video" : "../videos/kettle-bell.mp4"
